7b51c3d79defaf3d790c1d74611d5bf1dbaa308d
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-11-26 21:09:05 +0500

    Task5

---
+++ Tyuiu.KonovalovaAO.Sprint5.Task6.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint5.Task6.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint5.Task6.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("869dd11b-a4fe-40a3-8420-1557d2e1c6d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KonovalovaAO.Sprint5.Task6.V23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KonovalovaAO.Sprint5.Task6.V23.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint5.Task6.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Коновалова А. О. | СМАРТб-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #5                                                              *");
+            Console.WriteLine("*Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #23                                                            *");
+            Console.WriteLine("* Выполнила: Коновалова Александра Олеговна | СМАРТб-23-1                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask6V23.txt. в котором есть    *");
+            Console.WriteLine("* набор символьных данных.Найти количество знаков тире в заданной строке.*");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V23.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+            Console.ReadKey();
+        }
+    }
+}
